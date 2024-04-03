858a9bf76e04613ed165fbf5e6cec5d6c836be3f
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-04-04 01:42:13 +0900

    :memo: docs: 디렉토리 변경에 따른 스크립트문 수정

--- src/main/resources/script/SC.sh
+++ src/main/resources/script/SC.sh
@@ -2,8 +2,8 @@
 
 echo "채널 생성 시작"
 
-# Step 1: artifacts/channel 디렉토리로 이동
-cd ~/capstone-project-anbang/anbang-network/artifacts/channel
+# Step 1: 실행 파일이 있는 폴더로 이동.
+cd ~/anbang-network/channel
 
 # Step 1-2: docker-compose 실행 종료
 docker-compose down
